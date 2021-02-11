a1dcea64c790f381cf8ab501e1404f89f9ed1a42
moshes7 <m@m.com>
2021-02-11 18:19:35 +0200

    compare analyzer directories - first working version

--- analyze/compare.py
+++ analyze/compare.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-def compare_analyzer_dirs(dir1, dir2, output_dir, concat_type='horizontal', sfx_output='png', resize_factor=1, generate_video=True):
+def compare_analyzer_dirs(dir1, dir2, output_dir, concat_type='horizontal', sfx_list=['.png', '.jpg', '.JPEG'], sfx_output='png', resize_factor=1, generate_video=True):
     """
     Compare 2 analyzer directories.
 
@@ -31,18 +31,23 @@ def compare_analyzer_dirs(dir1, dir2, output_dir, concat_type='horizontal', sfx_
     # compare images
     images_dir1 = os.path.join(dir1, 'images')
     images_dir2 = os.path.join(dir2, 'images')
-    compare_images_in_two_dirs(images_dir1, images_dir2, output_dir, concat_type=concat_type, sfx_output=sfx_output,
-                               resize_factor=resize_factor, generate_video=generate_video)
+    output_images_dir = os.path.join(output_dir, 'images')
+    os.makedirs(output_images_dir, exist_ok=True)
+    compare_images_in_two_dirs(images_dir1, images_dir2, output_images_dir, names_type='int', concat_type=concat_type,
+                               sfx_list=sfx_list, sfx_output=sfx_output, resize_factor=resize_factor,
+                               generate_video=generate_video, fps=10)
 
     # compare metrics
-    compare_images_in_two_dirs(dir1, dir2, output_dir, concat_type=concat_type, sfx_output=sfx_output,
-                               resize_factor=1, generate_video=False)
+    compare_images_in_two_dirs(dir1, dir2, output_dir, names_type='str', concat_type=concat_type, sfx_list=sfx_list,
+                               sfx_output=sfx_output, resize_factor=1, generate_video=False)
 
     return
 
 
 
-def compare_images_in_two_dirs(dir1, dir2, output_dir, concat_type='horizontal', sfx_output='png', resize_factor=1, generate_video=True):
+def compare_images_in_two_dirs(dir1, dir2, output_dir, names_type='int', concat_type='horizontal', 
+                               sfx_list=['.png', '.jpg', '.JPEG'], sfx_output='png', resize_factor=1,
+                               generate_video=True, fps=10):
     """
     Compare corresponding images from 2 differect directories by concatenating them to 1 image and saving the result in
     output_dir.
@@ -55,6 +60,8 @@ def compare_images_in_two_dirs(dir1, dir2, output_dir, concat_type='horizontal',
         Second directory.
     output_dir : str
         Output directory.
+    names_type : str, optional
+        File names type, one of {'int', 'str'}
     concat_type : str, optional
         Concatenation type, one of {'horizontal', 'vertical'}.
     sfx_output : str, optional
@@ -71,21 +78,30 @@ def compare_images_in_two_dirs(dir1, dir2, output_dir, concat_type='horizontal',
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
-    # get image numbers and paths in dict in the format {image_number: image_full_path}
-    images_dict_1 = {'{}'.format(int(image_name.split('.')[0])) : os.path.abspath(os.path.join(dir1, image_name))
-                     for image_name in os.listdir(dir1)}
-    images_dict_2 = {'{}'.format(int(image_name.split('.')[0])) : os.path.abspath(os.path.join(dir2, image_name))
-                     for image_name in os.listdir(dir2)}
+    if names_type == 'int':
+        # get image numbers and paths in dict in the format {image_number: image_full_path}
+        images_dict_1 = {'{}'.format(int(image_name.split('.')[0])) : os.path.abspath(os.path.join(dir1, image_name))
+                         for image_name in os.listdir(dir1) if os.path.splitext(image_name)[1] in sfx_list}
+        images_dict_2 = {'{}'.format(int(image_name.split('.')[0])) : os.path.abspath(os.path.join(dir2, image_name))
+                         for image_name in os.listdir(dir2) if os.path.splitext(image_name)[1] in sfx_list}
 
-    # find common keys
-    keys1 = np.fromiter(images_dict_1.keys(), dtype=np.int)
-    keys2 = np.fromiter(images_dict_2.keys(), dtype=np.int)
-    keys = np.intersect1d(keys1, keys2)
-    keys = np.sort(keys)
+        # find common keys
+        keys1 = np.fromiter(images_dict_1.keys(), dtype=np.int)
+        keys2 = np.fromiter(images_dict_2.keys(), dtype=np.int)
+        keys = np.intersect1d(keys1, keys2)
+        keys = np.sort(keys)
 
-    # generate output images directory
-    output_images_dir = os.path.join(output_dir, 'images')
-    os.makedirs(output_images_dir, exist_ok=True)
+    elif names_type == 'str':
+        # get image numbers and paths in dict in the format {image_number: image_full_path}
+        images_dict_1 = {'{}'.format(image_name.split('.')[0]): os.path.abspath(os.path.join(dir1, image_name))
+                         for image_name in os.listdir(dir1) if os.path.splitext(image_name)[1] in sfx_list}
+        images_dict_2 = {'{}'.format(image_name.split('.')[0]): os.path.abspath(os.path.join(dir2, image_name))
+                         for image_name in os.listdir(dir2) if os.path.splitext(image_name)[1] in sfx_list}
+
+        # find common keys
+        keys1 = set(images_dict_1.keys())
+        keys2 = set(images_dict_2.keys())
+        keys = keys1.intersection(keys2)
 
     # iterate over keys
     for n, key in tqdm(enumerate(keys)):
@@ -99,16 +115,17 @@ def compare_images_in_two_dirs(dir1, dir2, output_dir, concat_type='horizontal',
             img1 = cv2.resize(img1, None, None, fx=resize_factor, fy=resize_factor)
             img2 = cv2.resize(img2, None, None, fx=resize_factor, fy=resize_factor)
 
-        # concatenate
+        # concatenate images
         img_concat = concatenate_images(img1, img2, concat_type=concat_type)
 
         # save concatenate image
-        img_name = os.path.join(output_images_dir, '{:06d}.{}'.format(key, sfx_output))
+        key_str = '{:06d}'.format(key) if names_type == 'int' else key
+        img_name = os.path.join(output_dir, '{}.{}'.format(key_str, sfx_output))
         cv2.imwrite(img_name, img_concat)
 
     if generate_video:
-        output_file = os.path.join(output_dir, 'video.mp4')
-        generate_video_from_images(output_images_dir, output_file, img_sfx=sfx_output)
+        output_file = os.path.join(output_dir, '..', 'video.mp4')  # make video one path level above output_dir
+        generate_video_from_images(output_dir, output_file, fps=fps)
 
     return
 
@@ -188,7 +205,7 @@ def match_image_size(img1, img2):
 
     return img1_pad, img2_pad
 
-def generate_video_from_images(images_dir, output_file, img_sfx='jpg', fps=10, max_frames_num=None):
+def generate_video_from_images(images_dir, output_file, fps=10, max_frames_num=None):
     """
     Generate video file from images diretory.
 
@@ -198,8 +215,6 @@ def generate_video_from_images(images_dir, output_file, img_sfx='jpg', fps=10, m
         Images directory.
     output_file : str
         Output video file name.
-    img_sfx : str, optional
-        Images suffix.
     fps : int, optional
         Video frame rate.
     max_frames_num : int, optional
@@ -210,7 +225,7 @@ def generate_video_from_images(images_dir, output_file, img_sfx='jpg', fps=10, m
     None.
     """
     # get image list
-    image_list = [os.path.join(images_dir, file_name) for file_name in os.listdir(images_dir)] #if file_name.endswith(img_sfx)]
+    image_list = sorted([os.path.join(images_dir, file_name) for file_name in os.listdir(images_dir)]) #if file_name.endswith(img_sfx)]
 
     if max_frames_num is not None:
         image_list = image_list[:max_frames_num]
