57e6879525cbfb6f92ec0783abbddff5e61796cd
AmitN1212 <amitn2003@gmail.com>
2021-09-02 10:52:51 +0300

    init gui

--- analyze/examples/viewer_example.py
+++ analyze/examples/viewer_example.py
@@ -1,29 +1,31 @@
 import os
-
 from analyze.analyzer import Analyzer
 from analyze.viewer import AnalyzerViewer
 import analyze.viewer2
-if __name__ == '__main__':
+import streamlit as st
 
-    import os
-    import panel as pn
-    pn.extension()
 
-    # load analyzer
-
+@st.cache(show_spinner=False)
+def load_analzyer(path):
     base_dir = os.path.dirname(__file__)
     relative_data_dir = '../tests/data/ILSVRC2015_00078000'
     data_dir = os.path.join(base_dir, relative_data_dir)
-    analyzer_file = os.path.join(data_dir, 'analyzer.p')
+    analyzer_file = os.path.join(data_dir, path)
     analyzer = Analyzer.load(analyzer_file, load_images_from_dir=False)
     os.chdir('..')  # go one level up
 
-    # initialize viewer
+    for i in range(len(analyzer.data)):
+        analyzer.data[i][
+            "image_path"] = "C:\\Users\\AMITNIM\\PycharmProjects\\odeval\\analyze\\tests\\data\\ILSVRC2015_00078000" \
+                            "\\images\\" + os.path.basename( analyzer.data[i]["image_path"])
+    return analyzer
+
+
+if __name__ == '__main__':
+    #analyzer = load_analzyer('analyzer.p')
+    analyzer  = 0
     # viewer = AnalyzerViewer(analyzer, resize_factor=2.)
-    #
-    # # view analyzer
     # viewer.view()
-    viewer: analyze.viewer2.Viewer = analyze.viewer2.Viewer()
-    viewer.run()
+    analyze.viewer2.run(analyzer)
 
     print('Done!')
--- analyze/viewer2.py
+++ analyze/viewer2.py
@@ -1,10 +1,51 @@
-import streamlit
+import streamlit as st
+import cv2
+import numpy as np
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
 
 
-class Viewer:
-    def __init__(self):
-        streamlit.title("Test")
+def run(analyzer):
+    st.title("Test")
+    app_mode = st.sidebar.selectbox("Mode", ["Summary", "Frame Viewer", "EXIT"])
+    if app_mode == "Summary":
+        st.write("reach")
+    elif app_mode == "Frame Viewer":
+        # st.image(load_image(analyzer.data[st.session_state.count]["image_path"]).astype(np.uint8),
+        #          use_column_width=True)
+        # b1, _, b2 = st.columns(3)
+        # b1.button("Back", on_click=click)
+        # b2.button("Forward", on_click=click)
+        st.write("reach2")
 
-    def run(self) -> None:
-        while 1:
-            pass
+
+
+# class Viewer:
+#     def __init__(self, analyzer):
+#         st.title("Test")
+#         self.analyzer = analyzer
+#         self.app_mode = st.sidebar.selectbox("Mode", ["Summary", "Frame Viewer", "EXIT"])
+#         self.current_id: int = 0
+#         self.last_id = 1
+#
+#     def run(self) -> None:
+#         while 1:
+#             if self.app_mode == "Summary":
+#                 if self.last_id != self.current_id:
+#                     st.write("testy")
+#             elif self.app_mode == "Frame Viewer":
+#                 if self.last_id != self.current_id:
+#                     self.init_page_image()
+#             elif self.app_mode == "EXIT":
+#                 pass
+#
+
+@st.cache(show_spinner=False)
+def load_image(path: str):
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    return image
+
+
+def click():
+    st.session_state.count += 1
